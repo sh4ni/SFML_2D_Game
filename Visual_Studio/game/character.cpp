e5310d389a219a29130bc7026bf3b1e7f712856b
Filip <pixerl0n@gmail.com>
2013-03-04 22:07:02 +0100

    small cleanup

--- Visual_Studio/game/character.cpp
+++ Visual_Studio/game/character.cpp
@@ -1,18 +1,11 @@
 #include "character.h"
 
 Character::Character(){
-	Init();
+	
 };
 Character::~Character(){
 
 };
-
-void Character::Init(){
-	
-}
-//void Character::Update(float ElapsedTime){
-//
-//}
 
 float Character::getPosX(void){
 	return this->PosX;
@@ -20,10 +13,14 @@ float Character::getPosX(void){
 float Character::getPosY(void){
 	return this->PosY;
 }
-void Character::setPosition(float x, float y){
+void Character::setPosition( float x, float y){
     this->PosX = x;
     this->PosY = y;
 	sprite.setPosition(x,y);
+}
+void Character::setMapSize( int xMax, int yMax){
+	this->MapSize.x = xMax;
+	this->MapSize.y = yMax;
 }
 
 float Character::getSpeed(void){
@@ -47,7 +44,6 @@ std::string Character::getName(void){
 	return this->Name;
 }
 
-
 int Character::getHealth(void){
 	return this->Health;
 }
@@ -66,11 +62,55 @@ void Character::setColMap(sf::IntRect***& ColMap){
 	this->ColMap = ColMap;
 };
 
-/*
-void Character::setBlockControl(bool block){
-	this->blockControl = block;
-	#ifdef DEBUGINFO
-		std::cout << "block " << block << std::endl;
-	#endif
+int Character::calcDamage(int damage,int level){
+	int levelDif = level-this->Lvl;
+    float multi = 1.f;
+    if(levelDif > 0){   // Gegner hat einen höhreren Level
+		if(levelDif >10) levelDif = 10;
+			multi += (float)levelDif*0.025f;
+		}
+		else if(levelDif < 0){ // Gegner hat einen niedrigeren Level
+			if(levelDif < -10) levelDif = -10;
+			multi += (float)levelDif*0.1f;
+		}
+		damage = (int)((float)damage * multi);
+		if ( damage <= 0 ){
+			damage = 1;
+		}
+	return damage;
 }
-*/
+
+void Character::damageText(int damage, char type){
+	Schrift E1;
+	std::ostringstream dmgString;
+	if( type == 'e') dmgString << "XP +";
+	else if( type == 'l') dmgString << "LEVEL ";
+		
+	dmgString << damage;
+	E1.Init(PosX,PosY,dmgString.str());
+	E1.printText.setStyle(sf::Text::Bold);
+	
+	if( type == 'm') E1.printText.setColor(sf::Color(0xFF,0xFF,0x00));
+	else if( type == 'p') E1.printText.setColor(sf::Color(0xFF,0x00,0x00));
+	else if( type == 'e') E1.printText.setColor(sf::Color(0xFF,0x00,0xFF));
+	else if( type == 'l'){
+		E1.printText.setCharacterSize(100);
+		E1.printText.setColor(sf::Color(0xFF,0x88,0x00));
+	}
+	
+	E1.printText.setOrigin(E1.printText.getGlobalBounds().width/2, 0);
+	dmgText.push_back(E1);
+}
+
+void Character::Render(sf::RenderWindow &renderWindow, bool drawEnemy = true){
+	if(drawEnemy) renderWindow.draw(sprite);
+
+	for( unsigned i=0; i<dmgText.size(); i++){
+		dmgText[i].printText.setPosition(dmgText[i].printText.getPosition().x, dmgText[i].printText.getPosition().y-1 );
+		dmgText[i].printText.setColor(sf::Color(dmgText[i].printText.getColor().r,dmgText[i].printText.getColor().g,dmgText[i].printText.getColor().b,dmgText[i].printText.getColor().a-4));
+		dmgText[i].Render(renderWindow);
+		if( dmgText[i].printText.getColor().a < 10 ){
+			dmgText.erase(dmgText.begin()+i);
+		}
+	}
+}
